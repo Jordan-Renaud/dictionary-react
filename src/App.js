79c2066f4b97db12f84e870c5d0aebea5edd8fe8
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-04-19 13:19:51 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ function App() {
   return (
     <div className="App">
       <Dictionary />
-      <footer>Code by Jordan Renaud</footer>
+      <footer>
+        <p>Code by Jordan Renaud</p>
+        <a
+          href="https://github.com/Jordan-Renaud/dictionary-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out the github
+        </a>
+      </footer>
     </div>
   );
 }
