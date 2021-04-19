95b39b32c62bbd33804ecf3dfe02462e41228328
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-04-19 11:01:07 +0100

    add api call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [word, setWord] = useState("");
@@ -11,6 +12,13 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${word} def`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data);
   }
 
   return (
